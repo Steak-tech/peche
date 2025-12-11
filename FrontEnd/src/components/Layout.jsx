7c0c14c454dd2dff84c1adae458f2b6e69e54520
Camille Kluck <geminy6254.jeux@gmail.com>
2025-12-11 14:34:32 +0100

    Commit Camille Nav style + modal

--- FrontEnd/src/components/Layout.jsx
+++ FrontEnd/src/components/Layout.jsx
@@ -1,16 +1,57 @@
+import { useState } from "react";
 import Nav from "./Nav";
-import { IoMdHome } from "react-icons/io";
+import { IoMdHome, IoMdClose, IoIosArrowForward } from "react-icons/io";
 import { IoAddCircle } from "react-icons/io5";
 import { FaUserCircle } from "react-icons/fa";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import FishCommonsModal from "./commons/FishCommonsModal";
 
 export default function Layout() {
+  const [openModal, setOpenModal] = useState(false);
+  const navigate = useNavigate();
+
+  const navItems = [
+    { icon: IoMdHome, url: "/" },
+    {
+      icon: IoAddCircle,
+      action: () => {
+        setOpenModal(true);
+      },
+    },
+    { icon: FaUserCircle, url: "/user" },
+  ];
+
   return (
     <>
-      <Nav data={[{icon: IoMdHome, url:'/'}, {icon: IoAddCircle, url:'/sortie'}, {icon: FaUserCircle, url:'/user' }]} />
+      <Nav data={navItems} />
+
       <main className="w-[100vw] h-[100vh] box-border">
         <Outlet />
       </main>
+
+      <FishCommonsModal open={openModal} onClose={() => setOpenModal(false)}>
+        <div className="flex justify-between items-center mb-6">
+          <h2 className="text-lg font-bold">Ajouter une sortie</h2>
+          <button onClick={() => setOpenModal(false)}>
+            <IoMdClose />
+          </button>
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <IoIosArrowForward />
+            <a href="" className="py-2">
+              Enregistrer une sortie
+            </a>
+          </div>
+          <hr />
+          <div className="flex items-center gap-2 cursor-pointer">
+            <IoIosArrowForward />
+            <a href="/sortie" className="py-2">
+              Créer une sortie
+            </a>
+          </div>
+        </div>
+      </FishCommonsModal>
     </>
   );
 }
